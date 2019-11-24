760eb5e0fad1dac2440f3b7736f396f2ab7be937
kimchangkyu <kimchangkyu2@naver.com>
2019-11-24 11:55:46 +0900

    Simple Array Sum

---
+++ Algorithms/01.Warmup/002.Simple Array Sum.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+def simpleArraySum(ar):
+    result = 0
+    result = sum(ar)
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
