a6596d2be1b8b303d1502e79e001bd5c84079cbe
kimchangkyu <kimchangkyu2@naver.com>
2020-01-03 14:28:47 +0900

    Day 14: Scope

---
+++ Tutorials/30 Days of Code/015.Day 14 - Scope.py
@@ -0,0 +1,19 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+        self.maximumDifference = 0
+
+	# Add your code here
+    def computeDifference(self):
+        for i in range(len(self.__elements)):
+            for j in range(i + 1, len(self.__elements)):
+                difference = abs(self.__elements[i] - self.__elements[j])
+                self.maximumDifference = max(difference, self.maximumDifference)
+        
+
+# End of Difference class
+_ = input()
+a = [int(e) for e in input().split(' ')]
+d = Difference(a)
+d.computeDifference()
+print(d.maximumDifference)
